ae7c1bbd6943a643b7fa442f05eabea930779bf9
Andrew Soden <andrewsoden@hotmail.com>
2019-01-09 18:51:52 +1000

    Updates

---
+++ precache-manifest.9e718548309f6185a207e37c05c50ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b59eabbfdf4abdc2df1a",
+    "url": "/toast_recipes/static/js/runtime~main.b59eabbf.js"
+  },
+  {
+    "revision": "fe31f37854148b53ccd8",
+    "url": "/toast_recipes/static/js/main.fe31f378.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/toast_recipes/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "fe31f37854148b53ccd8",
+    "url": "/toast_recipes/static/css/main.995d8e01.chunk.css"
+  },
+  {
+    "revision": "8eab136a1dbde73c574325a7cae9dcb5",
+    "url": "/toast_recipes/index.html"
+  }
+];
