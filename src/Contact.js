0cae01d05aaa1ed69a7f2049758142ac70900ef1
Andrew Soden <andrewsoden@hotmail.com>
2019-01-14 18:40:10 +1000

    Contact form validation work

--- src/Contact.js
+++ src/Contact.js
@@ -9,48 +9,131 @@ class Contact extends Component {
       email: '',
       contactNumber: '',
       password: '',
-      reEnterPassword: ''
+      reEnterPassword: '',
+      formValid: false
     }
+  }
+
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value},
+                  () => { this.validateField(name, value) });
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+
+    switch(fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? '': ' is too short';
+        break;
+      default:
+        break;
+    }
+    this.setState({formErrors: fieldValidationErrors,
+                    emailValid,
+                    passwordValid
+                  }, this.validateForm);
+  }
+
+  validateForm() {
+    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+  }
+
+  errorClass(error) {
+    return(error.length === 0 ? '' : 'has-error');
   }
   
   render() {
     return (
       <div>
         <h1>Contact</h1>
+        <div className="panel panel-default">
+        </div>
         <form className="contactForm">
           <label>
             First Name:
-          <input type="text" name="firstName" />
+          <input
+            type="text"
+            required
+            name="firstName"
+            placeholder="First Name"
+            value={this.state.firstName}
+            onChange={this.handleUserInput}
+          />
           </label>
           <br />
 
           <label>
             Surname:
-          <input type="text" name="surname" />
+          <input
+            type="text"
+            required
+            name="surname"
+            placeholder="Surname"
+            value={this.state.surname}
+            onChange={this.handleUserInput}
+          />
           </label>
           <br />
           
           <label>
             Email:
-          <input type="email" name="email" />
+          <input 
+            type="email"
+            required
+            name="email" 
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleUserInput}
+          />
           </label>
           <br />
           
           <label>
             Contact Number:
-          <input type="number" name="contactNumber" />
+          <input 
+            type="number" 
+            required 
+            name="contactNumber"
+            placeholder="Contact Number"
+            value={this.state.contactNumber}
+            onChange={this.handleUserInput}
+          />
           </label>
           <br />
           
           <label>
             Password:
-          <input type="password" name="password" />
+          <input 
+            type="password" 
+            required 
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleUserInput}
+          />
           </label>
           <br />
           
           <label>
             Re-enter Password:
-          <input type="password" name="reEnterPassword" />
+          <input 
+            type="password" 
+            required 
+            name="reEnterPassword" 
+            placeholder="Re-Enter Password"
+            value={this.state.reEnterPassword}
+            onChange={this.handleUserInput}
+          />
           </label>
           <br />
 
@@ -76,7 +159,12 @@ class Contact extends Component {
 
           <label>
             Comments:<br />
-            <textarea name="comments" rows="10" cols="30">
+            <textarea
+              name="comments"
+              rows="10"
+              cols="30"
+              placeholder="Anything you wish to let us know?"
+            >
             </textarea>
           </label>
           <br />
