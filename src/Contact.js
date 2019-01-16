f2c0c7f7159243c8026d07e7d67bef72fcde0c73
Andrew Soden <andrewsoden@hotmail.com>
2019-01-16 20:17:27 +1000

    New approach to Contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Form submitted');
+    event.preventDefault();
+    this.setState({value: ""});
+  }
   
   render() {
     return (
@@ -8,7 +27,7 @@ class Contact extends Component {
         <h1>Contact</h1>
         <div className="panel panel-default">
         </div>
-        <form className="contactForm">
+        <form className="contactForm" onSubmit={this.handleSubmit}>
           <label>
             First Name:
           <input
@@ -16,94 +35,9 @@ class Contact extends Component {
             required
             name="firstName"
             placeholder="First Name"
+            value={this.state.value}
+            onChange={this.handleChange}
           />
-          </label>
-          <br />
-
-          <label>
-            Surname:
-          <input
-            type="text"
-            required
-            name="surname"
-            placeholder="Surname"
-          />
-          </label>
-          <br />
-          
-          <label>
-            Email:
-          <input 
-            type="email"
-            required
-            name="email" 
-            placeholder="Email"
-          />
-          </label>
-          <br />
-          
-          <label>
-            Contact Number:
-          <input 
-            type="number" 
-            required 
-            name="contactNumber"
-            placeholder="Contact Number"
-          />
-          </label>
-          <br />
-          
-          <label>
-            Password:
-          <input 
-            type="password" 
-            required 
-            name="password"
-            placeholder="Password"
-          />
-          </label>
-          <br />
-          
-          <label>
-            Re-enter Password:
-          <input 
-            type="password" 
-            required 
-            name="reEnterPassword" 
-            placeholder="Re-Enter Password"
-          />
-          </label>
-          <br />
-
-          <label>
-            Preferred Contact Method:<br />
-            <input type="radio" name="preferredContact" value="either" checked />Either
-            <input type="radio" name="preferredContact" value="email" />Email
-            <input type="radio" name="preferredContact" value="mobile" />Phone
-          </label>
-          <br />
-
-          <label>
-            How Did You Hear About Us?:
-            <br />
-            <select name="howHeardAboutUs">
-              <option value="socialMedia">Social Media</option>
-              <option value="online">Online</option>
-              <option value="friendsOrFamily">Friends/family</option>
-              <option value="other">Other</option>
-            </select>
-          </label>
-          <br />
-
-          <label>
-            Comments:<br />
-            <textarea
-              name="comments"
-              rows="10"
-              cols="30"
-              placeholder="Anything you wish to let us know?"
-            >
-            </textarea>
           </label>
           <br />
 
