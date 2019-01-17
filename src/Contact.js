d3d1b877edb12e3f470e9c0978553bfc7cff2b44
Andrew Soden <andrewsoden@hotmail.com>
2019-01-17 21:06:57 +1000

    Contact form validation

--- src/Contact.js
+++ src/Contact.js
@@ -12,39 +12,77 @@ class Contact extends Component {
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    //this.setState({value: event.target.value});
+    event.target.classList.add('active');
+
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    
+    this.showInputError(event.target.name);
   }
 
   handleSubmit(event) {
     alert('Form submitted');
     event.preventDefault();
-    this.setState({value: ""});
+    this.setState({firstName: ''});
+  }
+
+  showFormErrors() {
+    const inputs = document.querySelectorAll('input');
+    let isFormValid = true;
+    
+    inputs.forEach(input => {
+      input.classList.add('active');
+      
+      const isInputValid = this.showInputError(input.name);
+      
+      if (!isInputValid) {
+        isFormValid = false;
+      }
+    });
+    
+    return isFormValid;
+  }
+  
+  showInputError(refName) {
+    const validity = this.refs[refName].validity;
+    const label = document.getElementById(`${refName}Label`).textContent;
+    const error = document.getElementById(`${refName}Error`);
+        
+    if (!validity.valid) {
+      if (validity.valueMissing) {
+        error.textContent = `${label} is a required field`; 
+      }
+      return false;
+    }
+    
+    error.textContent = '';
+    return true;
   }
   
   render() {
     return (
       <div>
         <h1>Contact</h1>
-        <div className="panel panel-default">
-        </div>
-        <form className="contactForm" onSubmit={this.handleSubmit}>
-          <label>
-            First Name:
-          <input
+        <form novalidate>
+        <div className="form-group">
+          <label id="firstNameLabel">First Name</label>
+          <input className="form-control"
             type="text"
-            required
             name="firstName"
-            placeholder="First Name"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          </label>
-          <br />
+            ref="firstName"
+            pattern=".{2,}"
+            value={ this.state.firstName } 
+            onChange={ this.handleChange }
+            required />
+          <div className="error" id="firstNameError" />
+        </div>
 
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+        <button className="btn btn-primary"
+          onClick={this.handleSubmit}>Submit
+        </button>
+      </form>
       </div>
     );
   }
