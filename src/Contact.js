4d1c04137c56cf53cafef17218790b9936fef206
Andrew Soden <andrewsoden@hotmail.com>
2019-01-18 12:26:30 +1000

    Continued Contact form

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,13 @@ class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: ''
+      firstName: '',
+      surname: '',
+      email: '',
+      password: '',
+      rePassword: '',
+      contactNumber: '',
+      comment: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -12,7 +18,6 @@ class Contact extends Component {
   }
 
   handleChange(event) {
-    //this.setState({value: event.target.value});
     event.target.classList.add('active');
 
     this.setState({
@@ -41,7 +46,7 @@ class Contact extends Component {
         isFormValid = false;
       }
     });
-    
+
     return isFormValid;
   }
   
@@ -49,10 +54,20 @@ class Contact extends Component {
     const validity = this.refs[refName].validity;
     const label = document.getElementById(`${refName}Label`).textContent;
     const error = document.getElementById(`${refName}Error`);
+    const isFirstName = refName.indexOf('firstName') !== -1;
+    const isSurname = refName.indexOf('surname') !== -1;
+    const isPassword = refName.indexOf('password') !== -1;
+    const isRePassword = refName.indexOf('rePassword') !== -1;
         
     if (!validity.valid) {
       if (validity.valueMissing) {
         error.textContent = `${label} is a required field`; 
+      }  else if ((isFirstName || isSurname) && validity.patternMismatch) {
+        error.textContent = `${label} should be longer than 1 character`;
+      } else if (validity.typeMismatch) {
+        error.textContent = `${label} should be a valid email address`;
+      } else if ((isPassword || isRePassword) && validity.patternMismatch) {
+        error.textContent = `${label} should be longer than 7 characters`;
       }
       return false;
     }
@@ -66,22 +81,80 @@ class Contact extends Component {
       <div>
         <h1>Contact</h1>
         <form novalidate>
-        <div className="form-group">
-          <label id="firstNameLabel">First Name</label>
-          <input className="form-control"
-            type="text"
-            name="firstName"
-            ref="firstName"
-            pattern=".{2,}"
-            value={ this.state.firstName } 
-            onChange={ this.handleChange }
-            required />
-          <div className="error" id="firstNameError" />
-        </div>
+          <div className="form-group">
+            <label id="firstNameLabel">First Name</label>
+            <input className="form-control"
+              type="text"
+              name="firstName"
+              ref="firstName"
+              pattern=".{2,}"
+              value={this.state.firstName} 
+              onChange={this.handleChange}
+              required 
+              autoFocus
+            />
+            <div className="error" id="firstNameError" />
+          </div>
 
-        <button className="btn btn-primary"
-          onClick={this.handleSubmit}>Submit
-        </button>
+          <div className="form-group">
+            <label id="surnameLabel">Surname</label>
+            <input className="form-control"
+              type="text"
+              name="surname"
+              ref="surname"
+              pattern=".{2,}"
+              value={this.state.surname} 
+              onChange={this.handleChange}
+              required 
+            />
+            <div className="error" id="surnameError" />
+          </div>
+
+          <div className="form-group">
+            <label id="emailLabel">Email</label>
+            <input className="form-control"
+              type="email"
+              name="email"
+              ref="email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+              value={this.state.email} 
+              onChange={this.handleChange}
+              required 
+            />
+            <div className="error" id="emailError" />
+          </div>
+
+          <div className="form-group">
+            <label id="passwordLabel">Password</label>
+            <input className="form-control"
+              type="password"
+              name="password"
+              ref="password"
+              pattern=".{8,}"
+              value={this.state.password} 
+              onChange={this.handleChange}
+              required 
+            />
+            <div className="error" id="passwordError" />
+          </div>
+
+          <div className="form-group">
+            <label id="rePasswordLabel">Retype Password</label>
+            <input className="form-control"
+              type="password"
+              name="rePassword"
+              ref="rePassword"
+              pattern=".{8,}"
+              value={this.state.rePassword} 
+              onChange={this.handleChange}
+              required 
+            />
+            <div className="error" id="rePasswordError" />
+          </div>
+
+          <button className="btn btn-primary"
+            onClick={this.handleSubmit}>Submit
+          </button>
       </form>
       </div>
     );
