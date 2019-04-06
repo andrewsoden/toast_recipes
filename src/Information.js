f30473de99d9e0aaa25b0131af895d1ed56d231b
Andrew Soden <andrewsoden@hotmail.com>
2019-04-06 14:36:38 +1000

    Start working on information page contents

--- src/Information.js
+++ src/Information.js
@@ -6,6 +6,33 @@ class Information extends Component {
     return (
       <div className="page">
         <h2>Rejoice for this is the Information page</h2>
+        <a href="#recipeHeading1">Recipe #1</a><br />
+        <a href="recipeHeading2">Recipe #2</a><br />
+        <a href="recipeHeading3">Recipe #3</a><br />
+
+        <h3 id="recipeHeading2">Recipe #1</h3>
+        <table className="basicTable">
+          <tr>
+            <td>Information</td>
+            <td>Picture</td>
+          </tr>
+        </table>
+
+        <h3 id="recipeHeading2">Recipe #2</h3>
+        <table className="basicTable">
+          <tr>
+            <td>Information</td>
+            <td>Picture</td>
+          </tr>
+        </table>
+
+        <h3 id="recipeHeading3">Recipe #3</h3>
+        <table className="basicTable">
+          <tr>
+            <td>Information</td>
+            <td>Picture</td>
+          </tr>
+        </table>
       </div>
     );
   }
